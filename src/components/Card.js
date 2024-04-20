7e30e434ea896eff697cef94e8635fc2571e7145
Lucas Tan <sheng.x.tan@gmail.com>
2024-04-19 23:46:34 -0600

    refactor Card to be dynamic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,35 @@
 import { AiFillStar } from 'react-icons/ai';
 import { BsFillBagHeartFill } from 'react-icons/bs';
 
-function Card() {
+function Card({ img, title, star, reviews, newPrice, prevPrice }) {
     return (
         <section className="card">
-                <img
-                    src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-                    alt="Shoe"
-                    className="card-img"
-                />
-                <div className="card-details">
-                    <h3 className="card=details">
-                        Shoe
-                    </h3>
-                    <section className="card=reviews">
-                        <AiFillStar className="ratings-star"/>
-                        <AiFillStar className="ratings-star"/>
-                        <AiFillStar className="ratings-star"/>
-                        <AiFillStar className="ratings-star"/>
-                        <span className="total-reviews">4</span>
-                    </section>
-                    <section className="card-price">
-                        <div className="price">
-                            <del>
-                                $300
-                            </del>
-                            200
-                        </div>
-                        <div className="bag">
-                            <BsFillBagHeartFill className="bag-icon" />
-                        </div>
-                    </section>
-                </div>
-            </section>
+            <img
+                src={img}
+                alt={title}
+                className="card-img"
+            />
+            <div className="card-details">
+                <h3 className="card=details">
+                    {title}
+                </h3>
+                <section className="card=reviews">
+                    {star}{star}{star}{star}{star}
+                    <span className="total-reviews">5</span>
+                </section>
+                <section className="card-price">
+                    <div className="price">
+                        <del>
+                            {prevPrice}
+                        </del>
+                        {` $${newPrice}`}
+                    </div>
+                    <div className="bag">
+                        <BsFillBagHeartFill className="bag-icon" />
+                    </div>
+                </section>
+            </div>
+        </section>
     )
 }
 
